9dab1957de722a88e7b8c31e1908b7758de02347
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-02-17 05:16:20 +0530

    [Nikhil Kumar]:BalancedParentheses Program Added

---
+++ DataStructurePrograms/BalancedParentheses.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructurePrograms
+{
+    public class BalancedParentheses
+    {
+        public class StackOperations
+        {
+            char[] item=new char[100];
+            int top = -1;
+            
+            public bool Push(char element)
+            {
+                if (top == item.Length)
+                {
+                    return false;
+                }
+                else
+                {
+                    item[++top] = element;
+                    return true;
+                }
+            }
+            
+            public char Pop()
+            {
+                if (top == -1)
+                {
+                    Console.WriteLine("Stack Cannot be -ve");
+                    return '\0';
+                }
+                else
+                {
+                    char element = item[top];
+                    top--;
+                    return element;
+                }
+            }
+            
+            public bool ComparingParentheses(char elem1, char elem2)
+            {
+                if (elem1 == '[' && elem2 == ']')
+                    return true;
+                else if (elem1 == '{' && elem2 == '}')
+                    return true;
+                else if (elem1 == '(' && elem2 == ')')
+                    return true;
+                else
+                    return false;
+            }
+            
+            public bool Balanced(char[] array)
+            {
+                item = new char[array.Length];
+                bool flag = true;
+                if (array.Length == 0)
+                    return true;
+
+                for (int i = 0; i < array.Length; i++)
+                {
+
+                    if (array[i] == '{' || array[i] == '(' || array[i] == '[')
+                    {
+                        Push(array[i]);
+                        flag = false;
+                    }
+                    if (array[i] == '}' || array[i] == ']' || array[i] == ')')
+                    {
+                        if (ComparingParentheses(Pop(), array[i]))
+                            flag = true;
+                        else
+                            return false;
+                    }
+                }
+                return flag;
+            }
+        }
+    }
+}
--- DataStructurePrograms/Program.cs
+++ DataStructurePrograms/Program.cs
@@ -11,6 +11,39 @@ namespace DataStructurePrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to DataStructure Program");
+            Console.WriteLine("1.Balanced Parentheses" +
+                              "\n2.Exit");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose Program");
+                int choose=Convert.ToInt32(Console.ReadLine());
+
+                switch (choose)
+                {
+                    case 1:
+                        char[] arr = "(5+6)∗(7+8)/(4+3)(5+6)∗(7+8)/(4+3)".ToCharArray();
+                        BalancedParentheses.StackOperations stackOperations = new BalancedParentheses.StackOperations();
+                        bool balanced = stackOperations.Balanced(arr);
+                        if (balanced)
+                        {
+                            Console.WriteLine("Parantheses are Balanced");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Parantheses are not Balanced");
+                        }
+                        break;
+
+                    case 2:
+                        flag = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+                }
+            }
         }
     }
 }
